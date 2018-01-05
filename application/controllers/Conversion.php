f47c9acf0a742527bb2a7ea7fedccbfbedd15691
fantingsheng <fants0230@sina.com>
2018-01-05 12:49:13 +0800

    更新

---
+++ application/controllers/Conversion.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Conversion extends CI_Controller {
+
+
+	public function index()
+	{
+		redirect("https://qr.alipay.com/c1x00743yq2d0yetwbwca14");
+	}
+
+}
