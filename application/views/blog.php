7ecb6932deaffd31d9f685df20b0faf1f7ab8098
fantingsheng <fants0230@sina.com>
2018-01-07 22:14:08 +0800

    fix 中文

--- application/views/blog.php
+++ application/views/blog.php
@@ -80,71 +80,7 @@
     </div>
 </div>
 <!-- Shop Cart -->
-<div class="cart-wrapper">
-    <a href="#" class="shop-cart-close"></a>
-    <div class="cart-header">
-        <h3 class="shop-cart-title">购物车</h3>
-        <ul class="shop_table shop_table_cart">
-            <li class="cart_item clearfix">
-                <div class="product-thumbnail">
-                    <a href="shop-details.html"><img src="images/Shop/cart-product-thumb-1.jpg" alt="Product 1"/></a>
-                </div>
-                <div class="product-info">
-                    <div class="product-name">
-                        <a href="shop-details.html">the skinny</a>
-                    </div>
-                    <div class="product-size-color">
-                        <span>Small/Blue</span>
-                    </div>
-                    <div class="product-quantity spinner" data-trigger="spinner">
-                        <input type="text" class="qty" value="1" data-rule="quantity"/>
-                        <div class="spinner-controls">
-                            <a href="javascript:;" data-spin="up" class="spin-up">+</a>
-                            <a href="javascript:;" data-spin="down" class="spin-down">-</a>
-                        </div>
-                    </div>
-                    <div class="product-subtotal">
-                        <span>$175.00</span>
-                    </div>
-                </div>
-            </li>
-            <li class="cart_item clearfix">
-                <div class="product-thumbnail">
-                    <a href="shop-details.html"><img src="images/Shop/cart-product-thumb-2.jpg" alt="Product 2"/></a>
-                </div>
-                <div class="product-info">
-                    <div class="product-name">
-                        <a href="shop-details.html">the slouchy</a>
-                    </div>
-                    <div class="product-size-color">
-                        <span>Small/Blue</span>
-                    </div>
-                    <div class="product-quantity spinner" data-trigger="spinner">
-                        <input type="text" class="qty" value="1" data-rule="quantity"/>
-                        <div class="spinner-controls">
-                            <a href="javascript:;" data-spin="up" class="spin-up">+</a>
-                            <a href="javascript:;" data-spin="down" class="spin-down">-</a>
-                        </div>
-                    </div>
-                    <div class="product-subtotal">
-                        <span>$150.00</span>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div class="cart-footer">
-        <div class="cart-wrap-total-checkout clearfix">
-            <div class="cart_total clearfix">
-                <span class="cart-total-text">Subtotal</span>
-                <span class="cart-total">$325.00</span>
-            </div>
-            <div class="proceed-to-checkout">
-                <a href="/checkout" class="checkout-button"><span>Check Out</span></a>
-            </div>
-        </div>
-    </div>
-</div>
+<?php include "common/shop-cart.php" ?>
 <!-- End Shop Cart -->
 
 <!-- main JS libs  -->
