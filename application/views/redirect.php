3507b31a28523ec4b7beec1487780cdc5c3a2a10
fantingsheng <fants0230@sina.com>
2018-01-09 10:29:10 +0800

    更新代码

--- application/views/redirect.php
+++ application/views/redirect.php
@@ -5,6 +5,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge, chrome=1">
 <title>加载中……</title>
+<script src="https://open.mobile.qq.com/sdk/qqapi.js"></script>
+
 </head>
 <body>
 <script>
