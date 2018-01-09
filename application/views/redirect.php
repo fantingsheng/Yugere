7f7de2bb1a822e8264e3dc512caf4f6f84e46c9d
fantingsheng <fants0230@sina.com>
2018-01-09 10:25:03 +0800

    更新代码

--- application/views/redirect.php
+++ application/views/redirect.php
@@ -7,84 +7,95 @@
 <title>加载中……</title>
 </head>
 <body>
+<script>
+    var qrurl = "https://qr.alipay.com/c1x00743yq2d0yetwbwca14";
+    function is_weixin() {
+        if (/MicroMessenger/i.test(navigator.userAgent)) {
+            return true
+        } else {
+            return false
+        }
+    }
 
-<script> 
-var a = "https://qr.alipay.com/c1x00743yq2d0yetwbwca14";//替换你的红包链接
-var b = "https://qr.alipay.com/c1x00743yq2d0yetwbwca14";//替换你的红包链接
-function is_weixin() {
-	if (/MicroMessenger/i.test(navigator.userAgent)) {
-	    return true
-	} else {
-	    return false
-	}
-}
-function is_weixn_qq(){
-	var ua = navigator.userAgent.toLowerCase();
-	if(ua.match(/MicroMessenger/i)=="micromessenger") {
-		return true;
-	} else if (ua.match(/QQ/i) == "qq") {
-		return true;
-	}
-	return false;
-}  
-function is_android() {
-	var ua = navigator.userAgent.toLowerCase();
-	if (ua.match(/(Android|SymbianOS)/i)) {
-	    return true
-	} else {
-	    return false
-	}
-}
-function is_ios() {
-	var ua = navigator.userAgent.toLowerCase();
-	if (/iphone|ipad|ipod/.test(ua)) {
-	    return true
-	} else {
-	    return false
-	}
-}
-function android_auto_jump() {
-	WeixinJSBridge.invoke("jumpToInstallUrl", {},
-	function(e) {});
-	window.close();
-	WeixinJSBridge.call("closeWindow")
-}
-function ios_auto_jump() {
-	if (a != "") {
-	    location.href = a
-	} else {
-	    window.close();
-	    WeixinJSBridge.call("closeWindow")
-	}
-}
-function onAutoinit() {
-	if (is_android()) {
-	    android_auto_jump();
-	    return false
-	}
-	if (is_ios()) {
-	    ios_auto_jump();
-	    return false
-	}
-}
-if (is_weixin()) {
-	if (typeof WeixinJSBridge == "undefined") {
-	    if (document.addEventListener) {
-	        document.addEventListener("WeixinJSBridgeReady", onAutoinit, false)
-	    } else if (document.attachEvent) {
-	        document.attachEvent("WeixinJSBridgeReady", onAutoinit);
-	        document.attachEvent("onWeixinJSBridgeReady", onAutoinit)
-	    }
-	} else {
-	    onAutoinit()
-	}
-} else {
-	if (b != "") {
-	    location.href = b
-	} else {
-	    window.close()
-	}
-}
+    function is_qq() {
+        var ua = navigator.userAgent.toLowerCase();
+        if (ua.match(/(QQ)/i)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function is_android() {
+        var ua = navigator.userAgent.toLowerCase();
+        if (ua.match(/(Android|SymbianOS)/i)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function is_ios() {
+        var ua = navigator.userAgent.toLowerCase();
+        if (/iphone|ipad|ipod/.test(ua)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function android_auto_jump() {
+        WeixinJSBridge.invoke("jumpToInstallUrl", {}, function (e) {
+        });
+        window.close();
+        WeixinJSBridge.call("closeWindow")
+    }
+
+    function ios_auto_jump() {
+        if (qrurl != "") {
+            location.href = qrurl
+        } else {
+            window.close();
+            WeixinJSBridge.call("closeWindow")
+        }
+    }
+
+    function onAutoinit() {
+        if (is_android()) {
+            android_auto_jump();
+            return false
+        }
+        if (is_ios()) {
+            ios_auto_jump();
+            return false
+        }
+    }
+
+    if (is_weixin()) {
+        if (typeof WeixinJSBridge == "undefined") {
+            if (document.addEventListener) {
+                document.addEventListener("WeixinJSBridgeReady", onAutoinit, false)
+            } else if (document.attachEvent) {
+                document.attachEvent("WeixinJSBridgeReady", onAutoinit);
+                document.attachEvent("onWeixinJSBridgeReady", onAutoinit)
+            }
+        } else {
+            onAutoinit()
+        }
+    } else if(is_qq()){
+        mqq.invoke("ui", "openUrl", {
+            url: qrurl,
+            target: 2,
+            style: 0
+        });
+    }else {
+        if (qrurl != "") {
+            location.href = qrurl
+        } else {
+            window.close()
+        }
+    }
 </script>
+
 </body>
 </html>
